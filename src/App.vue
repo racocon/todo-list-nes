d66d61a5137bcb6e32f315d00ff663b7c68a77f4
Racocon <diyanah.nadzree@live.com>
2021-02-13 20:58:24 +0800

    First commit, added todo list app

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+    <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+    
+    <div class="center-align">
+      <i class="nes-octocat animate"></i>
+    </div>
+
+    <br>
+    
+    <Todo/>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+html,
+body,
+pre,
+code,
+kbd,
+samp {
+  font-family: "Press Start 2P";
+}
+
+body {
+  background: #20262E;
+  padding: 20px;
+}
+
+#app {
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+  transition: all 0.2s;
+}
+
+li {
+  margin: 8px 0;
+}
+
+del {
+  color: rgba(0, 0, 0, 0.3);
+}
+
+.padding {
+  padding: 1px 7px 2px;
+}
+
+.flex {
+  display: flex;
+}
+
+.space {
+  flex-grow: 1;
+}
+
+.center-align {
+  text-align: center;
+}
+
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="todo">
+    <h2 class="center-align">{{ msg }}</h2>
+
+    <!-- Add todo list -->
+    <input 
+      type="text" 
+      class="nes-input" 
+      placeholder="Add todo..." 
+      v-on:keyup.enter="addTodo">
+
+    <ul>
+    <!-- Todo list -->
+      <li 
+        v-for="todo in todos"
+        :key="todo.id"
+        class="flex">
+
+        <label>
+          <input 
+            type="checkbox" 
+            class="nes-checkbox"
+            v-model="todo.done">
+          <span>&nbsp;</span>
+        </label>
+
+        <del v-if="todo.done">{{ todo.text }}</del>
+        <span v-else>{{ todo.text }}</span>
+        
+        <div class="space"></div>
+        <button 
+          class="nes-btn is-error padding"
+          v-on:click="removeTodo(todo.id)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data () {
+    return {
+      msg: 'TODOS:',
+
+      todos: [
+        { text: 'Create a todo list using Vue.js', done: true, id: Date.now() },
+        { text: 'Get scholarship from Krenovator', done: false, id: Date.now() + 1 },
+        { text: 'Reach Platinum rank in Apex', done: false, id: Date.now() + 2 }
+      ]
+    }
+  },
+  methods: {
+    // Add todo into the list
+    addTodo(event) {
+      const text = event.target.value
+      this.todos.push({text, done: false, id: Date.now()})
+      event.target.value = ''
+    },
+
+    // Remove todo from the list
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+
+    // Check todo as done in the list
+    check(todo) {
+      todo.done = !todo.done
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
